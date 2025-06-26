12a3160cb43fe47bb81f73b2500813ab8daf99f3
Dominid Muñoz Eslava <dominidzero@gmail.com>
2025-06-26 02:59:12 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ const App: React.FC = () => {
         <Route path="/instalacion" element={<InstalacionPage />} />
         <Route path="/mantenimiento" element={<MantenimientoPage />} />
       </Route>
-      <Route path="/reset-password" element={<ResetPasswordPage />} /> {/* <-- Nueva ruta */}
+      <Route path="/reset-password" element={<ResetPasswordPage />} /> 
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
